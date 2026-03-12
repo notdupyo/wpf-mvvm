bdaf8c59708e6addb9da4a9949c75017e570253f
notdupyo <not.dupyo@gmail.com>
2026-03-12 11:43:13 +0900

    ch08 과제 코드 추가

---
+++ WpfBasicApp/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasicApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Warrior warrior = new Warrior("아서", "전사", 10, 150, 10);
+
+            charName.Text = warrior.CharName;
+            charClass.Text = warrior.CharClass;
+            level.Text = warrior.Level.ToString();
+            Hp.Text = warrior.Hp.ToString();
+
+            
+        }
+
+        
+    }
+
+    public partial class Character
+    {
+        public string CharName { get; set; }
+        public string CharClass { get; set; }
+        public int Level { get; set; }
+        public int Hp { get; set; }
+        public int Mp { get; set; }
+
+        public Character(string charName, string charClass, int level, int hp, int mp)
+        {
+            CharName = charName;
+            CharClass = charClass;
+            Level = level;
+            Hp = hp;
+            Mp = mp;
+        }
+    }
+
+    public class Warrior : Character
+    {
+        public Warrior(string charName, string charClass, int level, int hp, int mp) : base(charName, charClass, level, hp, mp)
+        {
+        }
+    }
+
+    public class Mage : Character
+    {
+        public Mage(string charName, string charClass, int level, int hp, int mp) : base(charName, charClass, level, hp, mp)
+        {
+        }
+    }
+}
