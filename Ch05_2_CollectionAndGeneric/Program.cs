12f80e55e48efd850868dea7a70c8103bff5a865
notdupyo <not.dupyo@gmail.com>
2026-03-17 10:06:45 +0900

    학습 순서 수정, Ch05-2 colletion, Generic 학습 추가

---
+++ Ch05_2_CollectionAndGeneric/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ch05_2_CollectionAndGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
