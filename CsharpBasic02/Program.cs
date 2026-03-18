df0cd00df6efbb3b9937954acc1718a3a960c70a
notdupyo <not.dupyo@gmail.com>
2026-03-18 16:48:50 +0900

    ch10 람다식, LINQ 과제 코드 추가

--- CsharpBasic02/Program.cs
+++ CsharpBasic02/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+
+            // 예외처리 과제
             List<Character> lst = new List<Character>();
             try
             {
@@ -65,6 +67,61 @@
             {
                 Console.WriteLine($"{character.Name}, {character.CharacterClass}, {character.Level}, {character.Hp}, {character.Mp}");
             }
+
+            // 람다식, LINQ 과제
+            lst.Add(new Warrior("가나다", "전사", 2, 20, 3));
+            lst.Add(new Warrior("라마바", "전사", 20, 200, 30));
+            lst.Add(new Warrior("고노도", "전사", 12, 120, 6));
+            lst.Add(new Mage("신한", "마법사", 1, 10, 30));
+            lst.Add(new Mage("해리포터", "마법사", 33, 330, 660));
+            lst.Add(new Mage("전우치", "마법사", 50, 500, 1000));
+
+            // 레벨이 10 이상인 목록
+            List<Character> overLevel10List = lst.Where(character => character.Level >= 10).ToList();
+            Console.WriteLine("레벨 10 이상 목록: ");
+            foreach(Character character in overLevel10List)
+            {
+                Console.WriteLine($"[{character.CharacterClass}] {character.Name} LV.{character.Level} HP:{character.Hp} MP:{character.Mp}");
+            }
+
+            // 직업이 전사만 목록
+            List<Character> warriorList = lst.Where(character => character.CharacterClass == "전사").ToList();
+            Console.WriteLine("전사 목록:");
+            foreach (Character character in warriorList)
+            {
+                Console.WriteLine($"[{character.CharacterClass}] {character.Name} LV.{character.Level} HP:{character.Hp} MP:{character.Mp}");
+            }
+
+            // 레벨 내림차순 정렬
+            List<Character> sortDscByLevel = lst.OrderByDescending(character => character.Level).ToList();
+            Console.WriteLine("레벨 내림차순 DSC: ");
+            foreach (Character character in sortDscByLevel)
+            {
+                Console.WriteLine($"[{character.CharacterClass}] {character.Name} LV.{character.Level} HP:{character.Hp} MP:{character.Mp}");
+            }
+
+            // 이름만 추출
+            List<string> nameList = lst.Select(chracter => chracter.Name).ToList();
+            Console.WriteLine("이름만 출력: ");
+            foreach (string name in nameList)
+            {
+                Console.WriteLine($"{name}");
+            }
+
+            // 전체 캐릭터 평균레벨
+            double avgLevel = lst.Average(character => character.Level);
+            Console.WriteLine($"평균레벨: {avgLevel}");
+
+            // hp가 가장 높은 캐릭터
+            Character topHpCharacter = lst.OrderByDescending(character => character.Hp).FirstOrDefault();
+            Console.WriteLine($"최고 HP 캐릭터 {topHpCharacter.Name} - hp: {topHpCharacter.Hp}");
+
+            // 레벨 20이상인 마법사 존재 여부
+            bool hasOverLevel20Mage = lst
+                .Where(character => character.CharacterClass == "마법사")
+                .Any(character => character.Level >= 20);
+            Console.WriteLine($"레벨 20이상 마법사 여부 : {hasOverLevel20Mage}");
+
         }
     }
 
