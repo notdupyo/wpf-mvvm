e4576e48d9c93b763123d53bdbf60880578283e5
notdupyo <not.dupyo@gmail.com>
2026-03-27 18:41:01 +0900

    Ch14 ICommand 학습 예제 추가

--- Ch14_ViewModelAndICommand/MainWindow.xaml.cs
+++ Ch14_ViewModelAndICommand/MainWindow.xaml.cs
@@ -21,4 +21,25 @@ namespace Ch14_ViewModelAndICommand
             InitializeComponent();
         }
     }
+
+    public class RelayCommand:ICommand
+    {
+        private readonly Action<object> _excute;
+        private readonly Func<object, bool> _canExcute;
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<object> excute, Func<object, bool> canExcute = null)
+        {
+            _excute = excute ?? throw new ArgumentNullException(nameof(excute));
+            _canExcute = canExcute;
+        }
+
+        // 실행이 가능한지 여부를 반환하는 메서드
+        public bool CanExecute(object? parameter)
+        {
+            return _canExcute == null || _canExcute(parameter!);
+        }
+
+        
+    }
 }
